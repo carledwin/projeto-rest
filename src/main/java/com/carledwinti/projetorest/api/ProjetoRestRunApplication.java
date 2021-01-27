fdc54bebbe3279ec4f511d8730134d7793e67ad2
Carl Edwin Antonio Nascimento <carlinstr@gmail.com>
2021-01-27 20:02:53 -0300

    Initial commit

---
+++ src/main/java/com/carledwinti/projetorest/api/ProjetoRestRunApplication.java
@@ -0,0 +1,11 @@
+package com.carledwinti.projetorest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRestRunApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoRestRunApplication.class, args);
+    }
+}
